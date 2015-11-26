7fbd18c815742a3d60ba9acb7c37bb035d1d24d3
knutole <knutole@noerd.biz>
2015-11-26 19:58:59 +0100

    commented on run.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Docker
 
+1. Install Docker on any OS: https://docs.docker.com/v1.8/installation/
+2. Build docker container (`./build.sh`)
+3. Start docker container (`./run.sh`)
+4. Server is up! :)
--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker run -P -d timo/wwf
+
+# run the docker container (dev mode)
+docker run -p 443:443 -v /home/timo/docker:/home/timo -it timo/wwf
+
+# better for prod mode
+# docker run -p 443:443 -v /home/timo/docker:/home/timo -d timo/wwf
+
+
+# -p 443:443 				# means that the 443 port of the outside box (ie. your server) is tunnelled directly to the 443 port of the docker container
+# -v /home/timo/docker:/home/timo 	# mirrors a folder on the outside box to a folder inside the container, so container can read/write directly to outside box
+# -it / -d				# interactive mode (you'll be put inside a running container when starting). alternative is "-d", which is daemon mode, good for production
+# timo/wwf 				# name of the container, which is designated when building the container (see in build.sh)
